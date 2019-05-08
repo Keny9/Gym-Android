527e3267f628b2beec5bbc2c86b4c076b1a87b62
Massime <maxime.lussier1996@live.ca>
2019-05-08 13:03:25 -0400

    Interface Inscription

--- app/src/main/java/com/example/projetgym/InscriptionActivity.java
+++ app/src/main/java/com/example/projetgym/InscriptionActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class InscriptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
