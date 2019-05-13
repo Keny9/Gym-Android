fb855c39d1b0e022836d735d2bd57e4f533a0224
Guillaume <guillaume_scw@hotmail.com>
2019-05-13 13:39:03 -0400

    Menu Sur Accueil

--- app/src/main/java/com/example/projetgym/activity/AccueilActivity.java
+++ app/src/main/java/com/example/projetgym/activity/AccueilActivity.java
@@ -3,12 +3,16 @@ package com.example.projetgym.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.projetgym.R;
 import com.example.projetgym.helper.SQLiteHandler;
 import com.example.projetgym.helper.SessionManager;
+
+import android.view.Menu;
+import android.widget.Toast;
 
 public class AccueilActivity extends AppCompatActivity {
 
@@ -47,13 +51,33 @@ public class AccueilActivity extends AppCompatActivity {
         btnCours.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent newActivity = new Intent(getApplicationContext(), camera.class);
+                Intent newActivity = new Intent(getApplicationContext(), Cours_list.class);
                 startActivity(newActivity);
                 finish();
             }
         });
 
         boutonEvenement();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.settings){
+            Intent newActivity = new Intent(getApplicationContext(), preferences.class);
+            startActivity(newActivity);
+            finish();
+        }else if(item.getItemId() == R.id.help){
+            Toast.makeText(this, "Helpppppppppp me", Toast.LENGTH_SHORT).show();
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void boutonEvenement(){
---
+++ app/src/main/java/com/example/projetgym/activity/preferences.java
@@ -0,0 +1,44 @@
+package com.example.projetgym.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
+import com.example.projetgym.R;
+
+public class preferences extends AppCompatActivity {
+
+    private Button btnRetour;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+
+        btnRetour = findViewById(R.id.button);
+
+        btnRetour.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),AccueilActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void toastMsg(String msg) {
+
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+    public void displayToastMsg(View v) {
+
+        toastMsg("©Mickey Mouse Development Team, All Rights Reserved");
+
+    }
+}
