067fef17e0e70224b8130e7323b297f44ec67051
Guldrion <willurk@videotron.ca>
2019-05-15 11:45:34 -0400

    Interface choisir exercice 100%

--- php/exercice/gestionExercice.php
+++ php/exercice/gestionExercice.php
@@ -125,12 +125,18 @@ La fonction qui sert a ajouter un exercice dans la base de donnee
       trigger_error($conn->error);
     }
 
-    if ($result->num_rows > 0) {
+    if(mysqli_num_rows($result)==0){
+      $exercices = null;
+    }
+    else{
+      $exercices = $result;
+    }
+  /*  if ($result->num_rows > 0) {
 
       while($row = $result->fetch_assoc()) {
           $exercices[] = new Exercice($row['id_type'], $row['nom'], $row['description'], $row['image']);
         }
-    }
+    }*/
 
 
     return $exercices;
@@ -152,12 +158,19 @@ La fonction qui sert a ajouter un exercice dans la base de donnee
       trigger_error($conn->error);
     }
 
-    if ($result->num_rows > 0) {
+    if(mysqli_num_rows($result)==0){
+      $exercices = null;
+    }
+    else{
+      $exercices = $result;
+    }
+
+    /*if ($result->num_rows > 0) {
 
       while($row = $result->fetch_assoc()) {
           $exercices[] = new Exercice($row['id_type'], $row['nom'], $row['description'], $row['image']);
         }
-    }
+    }*/
 
 
     return $exercices;
